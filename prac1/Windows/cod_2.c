be461c48d8ada7c7f0fb56d595afe34275536e1a
Zentrx <sebastian.absalon2120@gmail.com>
2024-01-30 22:30:04 -0600

    Se creo la carpeta de windows para practica_1

---
+++ prac1/Windows/cod_2.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+
+int arrow(int size){
+    for(int k = 0; k < size / 2; k++){
+        for(int j = 0; j < size / 2; j++){
+            printf(" ");
+        } //Aqui esta bien
+        printf("*");//Aqui esta bien
+        for(int j = 0; j < (size - k)-1; j++){
+            printf(" ");
+        }//Aqui ya esta bien
+        for(int j = 0; j <= (2 * k); j++){
+            printf("*");
+        }
+        for(int j = 0; j < size / 2 - k; j++){
+            printf(" ");
+        }
+        printf("\n");//Parte de arriba completa
+    }
+    for(int i = 1; i <= size * 2; i++){
+        printf("*");
+    }//Parte de enmedio completa
+    printf("\n");
+    for(int k = 0; k < size / 2; k++){
+        for(int j = size / 2 - k; j <= size / 2; j++){
+            printf(" ");
+        }
+        for(int j = size-(2 * k)-2; j >= 1; j--){
+            printf("*");
+        }
+        for(int j = size; j < size + (size / 2) + k + 1; j++){
+            printf(" ");
+        }
+        printf("*");
+        printf("\n");
+    }
+}
+
+int main(){
+    int size;
+    char rand[20];
+    printf("Inserta el tamanio de la base de la felcha: ");
+    scanf("%d", &size);
+    if(size % 2 == 0){
+        size--;
+        printf("Se va a reducir el show\n");
+    }
+    arrow(size);
+}
