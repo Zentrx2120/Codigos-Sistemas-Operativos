3b70c34ba1d4cce3595e8fcace5f4a57526c8d84
Zentrx <sebastian.absalon2120@gmail.com>
2024-01-30 22:24:00 -0600

    Flechas cod_2

--- prac1/Linux/cod_2.c
+++ prac1/Linux/cod_2.c
@@ -1,58 +1,49 @@
-#include <stdio.h>
-#include <math.h>
+#include<stdio.h>
 
-void arrow(int);
-
-int main(){
-
-    int n;
-
-    printf("Escriba las bases de las flechas\n");
-    scanf("%d",&n);
-
-    if(n%2 !=0 ){
-        n+=1;
+int arrow(int size){
+    for(int k = 0; k < size / 2; k++){
+        for(int j = 0; j < size / 2; j++){
+            printf(" ");
+        } //Aqui esta bien
+        printf("*");//Aqui esta bien
+        for(int j = 0; j < (size - k)-1; j++){
+            printf(" ");
+        }//Aqui ya esta bien
+        for(int j = 0; j <= (2 * k); j++){
+            printf("*");
+        }
+        for(int j = 0; j < size / 2 - k; j++){
+            printf(" ");
+        }
+        printf("\n");//Parte de arriba completa
     }
-
-    arrow(n);
-
-    return 0;
+    for(int i = 1; i <= size * 2; i++){
+        printf("*");
+    }//Parte de enmedio completa
+    printf("\n");
+    for(int k = 0; k < size / 2; k++){
+        for(int j = size / 2 - k; j <= size / 2; j++){
+            printf(" ");
+        }
+        for(int j = size-(2 * k)-2; j >= 1; j--){
+            printf("*");
+        }
+        for(int j = size; j < size + (size / 2) + k + 1; j++){
+            printf(" ");
+        }
+        printf("*");
+        printf("\n");
+    }
 }
 
-void arrow(int x){
-    //printf("%d", x);
-    for(int k = 0; k < x / 2; k++){
-        for(int j = 0; j < x / 2 + 1; j++){
-            printf(" ");
-        }
-        printf("*");
-        for(int j = 0; j < x - k - 1; j++){
-            printf(" ");
-        }
-        for(int j = 0; j <= 0 + 2 * k; j++){
-            printf("*");
-        }
-        for(int j = 0; j < x / 2 - k; j++){
-            printf(" ");
-        }
-        printf("\n");
+int main(){
+    int size;
+    char rand[20];
+    printf("Inserta el tamanio de la base de la felcha: ");
+    scanf("%d", &size);
+    if(size % 2 == 0){
+        size--;
+        printf("Se va a reducir el show\n");
     }
-    for(int i = 0; i <= x * 2; i++){
-        printf("*");
-    }
-    printf("\n");
-    for(int k = 0; k <= x / 2; k++){
-        for(int j = x / 2 - k; j <= x / 2; j++){
-            printf(" ");
-        }
-        for(int j = x - 2 * k; j >= 1; j--){
-            printf("*");
-        }
-        for(int j = x; j < x + (x / 2) + k; j++){
-            printf(" ");
-        }
-        printf("*");
-        printf("\n");
-    }
-
+    arrow(size);
 }
